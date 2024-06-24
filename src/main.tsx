aaaaf8b0166325fb78059530b29ebb24efe76832
daniel-edustartech <daniel@edustartech.com>
2024-06-24 16:30:12 +0100

    created folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+if (process.env.NODE_ENV === 'development') {
+  const { server } = require('./mocks/browser');
+  server.listen();
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
